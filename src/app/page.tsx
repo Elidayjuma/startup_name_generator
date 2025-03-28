7a53a78c5b367fba6fbc59d6fe59fad6d8fbd044
elidayjuma <elidejuma@gmail.com>
2025-03-28 10:49:34 +0300

    refined code. kinda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 import Hero from "@/components/Hero";
-import Testimonials from "@/components/Testimonials";
-import Pricing from "@/components/Pricing/Pricing";
 import FAQ from "@/components/FAQ";
-import Logos from "@/components/Logos";
-import Benefits from "@/components/Benefits/Benefits";
 import Container from "@/components/Container";
-import Section from "@/components/Section";
-import Stats from "@/components/Stats";
 import CTA from "@/components/CTA";
 import FeatureCards from "@/components/FeaturedCards";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { siteDetails } from '@/data/siteDetails';
 
 const HomePage: React.FC = () => {
   return (
@@ -20,30 +13,8 @@ const HomePage: React.FC = () => {
       <Header />
       <Hero />
       <FeatureCards />
-      {/* <Logos /> */}
       <Container>
-        {/* <Benefits /> */}
-
-        {/* <Section
-          id="pricing"
-          title="Pricing"
-          description="Simple, transparent pricing. No surprises."
-        >
-          <Pricing />
-        </Section> */}
-
-        {/* <Section
-          id="testimonials"
-          title="What Our Clients Say"
-          description="Hear from those who have partnered with us."
-        >
-          <Testimonials />
-        </Section> */}
-
         <FAQ />
-
-        {/* <Stats /> */}
-
         <CTA />
       </Container>
       <Footer />
--- src/data/cta.ts
+++ src/data/cta.ts
@@ -1,5 +1,5 @@
 export const ctaDetails = {
-    heading: 'Join Over 1 Million Users To Kickstart Your Startup',
+    heading: 'Join Over 100k Users To Kickstart Your Startup',
     subheading: 'Your journey to a successful startup starts here. Check and get your name today and take the first step towards a brighter future!',
     appStoreUrl: '#',
     googlePlayUrl: '#'
